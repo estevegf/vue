6c02b8119cc0e78724924cb1baf48aa1beabb8e4
Esteve Genovard Ferriol <isestevegf@gmail.com>
2019-08-15 07:56:38 +0200

    Add CHANGELOG

--- src/router.js
+++ src/router.js
@@ -14,13 +14,13 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/education",
+      name: "education",
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (education.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "education" */ "./views/Education.vue")
     }
   ]
 });
---
+++ src/views/Education.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="education">
+    <h1>This is an education page</h1>
+  </div>
+</template>
